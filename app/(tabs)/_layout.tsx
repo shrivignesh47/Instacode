457b786132d660a078f17b2e99fad02542b8c71a
Shri Vignesh <shrivigneshshrivignesh4@gmail.com>
2025-06-29 20:06:04 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Tabs } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
-import { Home, Search, PlusSquare, MessageSquare, User } from 'lucide-react-native';
+import { Home, Search, SquarePlus as PlusSquare, MessageSquare, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
